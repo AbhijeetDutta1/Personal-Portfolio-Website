d3669499b161cd52a5c8911176a10c20b4e57242
Abhijeet Dutta <abhijeet@ucsb.edu>
2026-07-29 17:38:08 -0700

    Add /experience page

---
+++ app/experience/page.tsx
@@ -0,0 +1,10 @@
+import SwissShell from "@/components/swiss/SwissShell";
+import ExperienceTimeline from "@/components/experience/ExperienceTimeline";
+
+export default function ExperiencePage() {
+  return (
+    <SwissShell title="Experience">
+      <ExperienceTimeline />
+    </SwissShell>
+  );
+}
