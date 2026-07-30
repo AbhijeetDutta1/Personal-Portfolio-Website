76cf729fb3b8bfddb5ff2adf9d126b8ae7a6d466
Abhijeet Dutta <abhijeet@ucsb.edu>
2026-07-29 17:37:34 -0700

    Add /projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+import SwissShell from "@/components/swiss/SwissShell";
+import ProjectsExplorer from "@/components/projects/ProjectsExplorer";
+
+export default function ProjectsPage() {
+  return (
+    <SwissShell title="Projects">
+      <ProjectsExplorer />
+    </SwissShell>
+  );
+}
